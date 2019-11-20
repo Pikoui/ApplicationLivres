b32571e6dc9fa70adf1adf3e6fc7516784412f54
ISAsxm <huguesisabelle82@gmail.com>
2019-11-20 15:56:35 +0100

    aplivres

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'templates/header.php';
+?>
+<main>
+
+<h1 class="mainTitle">Ma bibliothèque personnelle</h1>
+
+<p class="para">Bienvenue sur cette application Web. Ici, vous retrouverez un recensement de divers livres.
+    Nous vous conseillerons certains de nos coups de coeur littéraires et proposerez un petit jeu dont VOUS serez le héros !
+</p>
+
+<input type="text" placeholder="Ok">
+<button id="button">Rechercher</button>
+
+<img src="histoire.jpg" class="fantastique">
+
+</main>
+
+
+<?php
+echo "Bonjour, je suis"
+?>
+
+
+<?php
+
+
+$myArray = [“xxx”, “yyy”];
+
+$myList = [“tomates”, “patates”, “salade”]
+
+var_dump ($myList);
+
+?>
+
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    
+
+    <header>
+
+        <div>
+            <ul class="topnav">
+                <li><a class="active" href="Accueil.html">Accueil</a></li>
+                <li><a href="presentation.html">Présentation</a></li>
+                <li><a href="api.php">Questionnaire</a></li>
+                <li><a href="game.php">Jeu</a></li>
+                <li><a class="right" href="form.php">Formulaire</a></li>
+            </ul>
+
+    </header>
